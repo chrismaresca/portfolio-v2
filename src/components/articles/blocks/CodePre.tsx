bddd28040cd281f5176535e064f5c923f549b819
Chris Maresca <chris@chrismaresca.dev>
2025-01-29 11:44:10 -0500

    Added code block functionality

---
+++ src/components/articles/blocks/CodePre.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { useState, useRef } from 'react';
+
+export const CodePre = ({ children }: { children: React.ReactNode }) => {
+  const textInput = useRef<HTMLPreElement>(null);
+  const [copied, setCopied] = useState(false);
+
+  const onCopy = () => {
+    setCopied(true);
+    navigator.clipboard.writeText(textInput.current?.textContent || '');
+    setTimeout(() => setCopied(false), 2000);
+  };
+
+  return (
+    <div className="overflow-hidden rounded-lg border border-gray-900 bg-[#0d1117] md:w-[95%]">
+      <div className="flex items-center justify-between border-b border-gray-900 px-4 py-3">
+        <span className="text-xs text-gray-400">Code</span>
+        <button
+          onClick={onCopy}
+          className="text-gray-400 hover:text-gray-300"
+          aria-label="Copy code"
+        >
+          {copied ? (
+            <svg className="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+            </svg>
+          ) : (
+            <svg className="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+            </svg>
+          )}
+        </button>
+      </div>
+      <pre ref={textInput} className="py-2 px-5">
+        {children}
+      </pre>
+    </div>
+  );
+};
--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -1,12 +1,33 @@
+// React
 import React from "react";
-import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
+
+// Next
 import Image from "next/image";
+
+// Utils
 import { cn } from "@/lib/utils";
 
-import {
-  Card,
-  CardContent,
-} from "@/components/ui/card";
+// MDX Components
+import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
+
+// Rehype Plugins
+import rehypeHighlight from "rehype-highlight";
+
+// Styles
+import "@/styles/atom-one-dark.css";
+
+// Custom Components
+import { CodePre } from "@/components/articles/blocks/CodePre";
+
+// Rehype Plugins
+const options: MDXRemoteProps["options"] = {
+  mdxOptions: {
+    remarkPlugins: [],
+    rehypePlugins: [rehypeHighlight],
+  },
+};
+
+import { Card, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 
@@ -80,10 +101,10 @@ const CODE: React.FC<ComponentProps> = ({ children, className, ...props }) => (
   </code>
 );
 
-const PRE: React.FC<ComponentProps> = ({ children, className, ...props }) => (
-  <pre {...props} className={cn("!mb-4 !mt-4 overflow-x-auto rounded-lg border bg-black p-4", className)}>
-    {children}
-  </pre>
+const PRE: React.FC<ComponentProps> = ({ children, ...props }) => (
+  <div className="!my-8">
+    <CodePre {...props}>{children}</CodePre>
+  </div>
 );
 
 const TABLE: React.FC<ComponentProps> = ({ children, className, ...props }) => (
@@ -106,9 +127,7 @@ const TD: React.FC<ComponentProps> = ({ children, className, ...props }) => (
   </td>
 );
 
-const IMG: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = ({ alt, src, ...props }) => (
-  <Image {...props} alt={alt || ""} src={src || ""} width={800} height={400} className="rounded-md border !my-4" />
-);
+const IMG: React.FC<React.ImgHTMLAttributes<HTMLImageElement>> = ({ alt, src, ...props }) => <Image {...props} alt={alt || ""} src={src || ""} width={800} height={400} className="rounded-md border !my-4" />;
 
 const components = {
   h1: H1,
@@ -140,8 +159,7 @@ interface CustomMDXProps extends Omit<MDXRemoteProps, "components"> {
 export function CustomMDX({ components: userComponents, ...props }: CustomMDXProps) {
   return (
     <div className="prose dark:prose-invert max-w-none space-y-0">
-      <MDXRemote {...props} components={{ ...components, ...(userComponents || {}) }} />
+      <MDXRemote {...props} components={{ ...components, ...(userComponents || {}) }} options={options} />
     </div>
   );
 }
-
